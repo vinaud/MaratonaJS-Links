8539f3efda2a2e754465c3713c92ba1dc01fc561
JoÃ£o Vinaud <leyenasd@gmail.com>
2020-06-29 10:59:25 -0300

    Criação do model account

--- backend/src/controllers/auth.js
+++ backend/src/controllers/auth.js
@@ -2,12 +2,15 @@ const express = require('express');
 
 const router = express.Router();
 
+const { Account } = require('../models');
+
 
 router.get('/sign-in', (request, response) =>{
     return response.json("Sign in"); 
 })
 
-router.get('/sign-up', (request, response) =>{
+router.get('/sign-up', async (request, response) =>{
+    const result = await Account.create({email: 'user@domain.com', password: '123456'});
     return response.json("Sign up");
 })
 
